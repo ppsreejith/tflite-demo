6a173a72c3da289741d8afd13d0b2d2a4fd29ac8
Sreejith Pp <mailz4sreejith@gmail.com>
2018-12-24 09:45:40 +0530

    Added App and Output files

--- App.js
+++ App.js
@@ -1,29 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
+import { RNCamera } from 'react-native-camera-testing';
+import outputs from './Output.json';
+import _ from 'lodash';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+let _currentInstant = 0;
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      output: ""
+    };
+  }
+
+  processOutput({data}) {
+    const probs = _.map(data, item => _.round(item/255.0, 0.02));
+    const orderedData = _.chain(data).zip(outputs).orderBy(0, 'desc').map(item => [_.round(item[0]/255.0, 2), item[1]]).value();
+    const outputData = _.chain(orderedData).take(3).map(item => `${item[1]}: ${item[0]}`).join('\n').value();
+    const time = Date.now() - (_currentInstant || Date.now());
+    const output = `Guesses:\n${outputData}\nTime:${time} ms`;
+    this.setState(state => ({
+      output
+    }));
+    _currentInstant = Date.now();
+  }
+  
   render() {
+    const modelParams = {
+      file: "mobilenet_v1_1.0_224_quant.tflite",
+      inputDimX: 224,
+      inputDimY: 224,
+      outputDim: 1001,
+      freqms: 0
+    };
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <RNCamera
+            ref={ref => {
+                this.camera = ref;
+              }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+            onModelProcessed={data => this.processOutput(data)}
+            modelParams={modelParams}
+        >
+          <Text style={styles.cameraText}>{this.state.output}</Text>
+        </RNCamera>
       </View>
     );
   }
@@ -32,18 +59,17 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  cameraText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
 });
